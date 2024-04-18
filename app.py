216570d3a689431f7495d97e64724197283dcf83
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-04-18 12:16:05 +0000

    updated import statements

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
-from submit_exercise import submit_exercise
-from retrieve_exercise import retrieve_exercise
+from flask import Flask, render_template
+from routes.submit_exercise import submit_exercise
+from routes.retrieve_exercise import retrieve_exercise
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit')
+def submit():
+    return render_template('submit.html')
+
+@app.route('/retrieve')
+def retrieve():
+    return render_template('retrieve.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
