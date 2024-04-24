0c6f5e9de9e87c4ef087f2627d5856a198949f7f
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-04-24 20:29:43 +0000

    updated config

--- db_config.py
+++ db_config.py
@@ -1,5 +1,5 @@
-import os
 from sqlalchemy import create_engine
+import os
 
 class DBConfig:
     @staticmethod
@@ -7,23 +7,7 @@ class DBConfig:
         """
         Creates a SQLAlchemy engine using environment variables for configuration.
         """
-        config = DBConfig.get_mysql_config()
-        engine = create_engine(**config)  # Unpack dictionary as arguments
-        return engine
-
-    @staticmethod
-    def get_mysql_config():
-        """
-        Retrieves MySQL connection details from environment variables.
-        """
-        config = {
-            'host': os.getenv('MYSQL_HOST'),
-            'user': os.getenv('MYSQL_USER'),
-            'password': os.getenv('MYSQL_PASSWORD'),
-            'database': os.getenv('MYSQL_DATABASE')
-        }
-        # Check if SSL mode is set
+        url = os.getenv('MYSQL_DATABASE_URL')
         ssl_mode = os.getenv('MYSQL_SSL_MODE')
-        if ssl_mode:
-            config['connect_args'] = {'sslmode': ssl_mode}
-        return config
+        engine = create_engine(url, connect_args={'sslmode': ssl_mode} if ssl_mode else None)
+        return engine
