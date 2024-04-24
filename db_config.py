7bed43871fb0277fa516bb8d7710a73b08dac1e0
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-04-24 19:42:55 +0000

    updated db_config

--- db_config.py
+++ db_config.py
@@ -1,21 +1,26 @@
 import os
 from sqlalchemy import create_engine
 
-
-def DBConfig():
-    config = {
-        'host': os.getenv('MYSQL_HOST'),
-        'user': os.getenv('MYSQL_USER'),
-        'password': os.getenv('MYSQL_PASSWORD'),
-        'database': os.getenv('MYSQL_DATABASE')
-    }
-    # Check if SSL mode is set
-    ssl_mode = os.getenv('MYSQL_SSL_MODE')
-    if ssl_mode:
-        config['connect_args'] = {'sslmode': ssl_mode}
-    return config
-
 def create_engine():
-    config = get_mysql_config()
-    engine = create_engine(**config)  # Unpack dictionary as arguments
-    return engine
+  """
+  Creates a SQLAlchemy engine using environment variables for configuration.
+  """
+  config = get_mysql_config()
+  engine = create_engine(**config)  # Unpack dictionary as arguments
+  return engine
+
+def get_mysql_config():
+  """
+  Retrieves MySQL connection details from environment variables.
+  """
+  config = {
+    'host': os.getenv('MYSQL_HOST'),
+    'user': os.getenv('MYSQL_USER'),
+    'password': os.getenv('MYSQL_PASSWORD'),
+    'database': os.getenv('MYSQL_DATABASE')
+  }
+  # Check if SSL mode is set
+  ssl_mode = os.getenv('MYSQL_SSL_MODE')
+  if ssl_mode:
+    config['connect_args'] = {'sslmode': ssl_mode}
+  return config
