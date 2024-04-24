60ed272f5aa7ce8a52deff2dc793ad6729714633
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-04-24 19:13:38 +0000

    updated db_config.py with SSL verify CA

--- db_config.py
+++ db_config.py
@@ -2,19 +2,20 @@ import os
 from sqlalchemy import create_engine
 
 class DBConfig:
-    @staticmethod
-    def get_database_url():
-        database_url = os.getenv('DATABASE_URL')
-        if not database_url:
-            raise ValueError("DATABASE_URL environment variable is not set")
-        return database_url
+  @staticmethod
+  def get_database_url():
+    database_url = os.getenv('DATABASE_URL')
+    if not database_url:
+      raise ValueError("DATABASE_URL environment variable is not set")
+    return database_url
 
-    @staticmethod
-    def create_engine():
-        database_url = DBConfig.get_database_url()
-        ssl_mode = "allow"  # Change this to match your SSL mode if needed
+  @staticmethod
+  def create_engine():
+    database_url = DBConfig.get_database_url()
+    ssl_mode = "verify-ca"  # Use the desired SSL mode
 
-        engine = create_engine(database_url, connect_args={'sslmode': ssl_mode})
-        return engine
+    # Use connect_args for dialect-specific arguments (assuming MySQL)
+    engine = create_engine(database_url, connect_args={'sslmode': ssl_mode})
+    return engine
 
 engine = DBConfig.create_engine()
